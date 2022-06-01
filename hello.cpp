4b2aec4bdcc2afbe2a2076b1812215487104ae93
User <user@Users-MacBook-Pro.local>
2022-06-01 12:30:28 +0300

    first commit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+int main() {
+    printf("Abcg");
+    printf("\b\b\b\b");
+    printf("****\n");
+    system ("clear");
+    printf("Abcg\n");
+
+    return 0;
+}

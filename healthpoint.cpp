9791184920f36ab6469a5b0aad7671d10d480304
User <user@Anastasia-MacBook-Pro.local>
2022-06-03 15:48:19 +0300

    update

--- healthpoint.cpp
+++ healthpoint.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main(){
-
+    srand(time(NULL));
     int health=100;
     int randomnumb;
 
     cout<<"Welcome to the BAR!"<<endl;
     for(; health>=0; ){
+        int randnum=rand()%4+1;
         cout<<"These are your health point: "<<health<<endl;
         cout<<"Enter the number 1-5: \t";
         cin>>randomnumb;
-        if (randomnumb==2||randomnumb==4){
+        if (randomnumb!=randnum){
             health-=5;
             cout<<"Ouch, you healthpoint -5"<<endl;
                 if(health==0){
@@ -21,8 +23,10 @@ int main(){
             continue;
         }
 
-        if (randomnumb==1||randomnumb==3||randomnumb==5){
+        if (randomnumb==randnum){
           cout<<"Phew... Good number!"<<endl;
+          cout<<"Your health piont +10"<<endl;
+          health+=10;
             continue;  
         }
     }
---
+++ rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+for(int x=0; x<10;x++){
+    for(int y=0; y<10;y++)
+        cout<<"0 ";
+    cout<<"\n";
+}
+return 0;
+}
